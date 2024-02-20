d28ae6b289066d76b7317edf3d9756f3b54cf367
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-20 12:08:28 +0200

    Update AnimatedCards.jsx

--- src/components/AnimatedCards.jsx
+++ src/components/AnimatedCards.jsx
@@ -1,10 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// react awsome reveal
+import { Slide, Fade } from "react-awesome-reveal";
+
+// assets
+import Photo1 from "../assets/pics/photo1.jpg";
+import Photo2 from "../assets/pics/photo2.jpg";
+import Photo3 from "../assets/pics/photo3.jpg";
+
+const CardsData = [
+  {
+    id: 1,
+    img: Photo1,
+    title: "Sunset",
+    desc: "Each character will appear one by one",
+  },
+  {
+    id: 2,
+    img: Photo2,
+    title: "Dog",
+    desc: "Each character will appear one by one",
+  },
+  {
+    id: 3,
+    img: Photo3,
+    title: "Sunrise",
+    desc: "Each character will appear one by one",
+  },
+];
+
 const AnimatedCards = () => {
   return (
-    <div>
-      <div className=""></div>
+    <div className="container mx-auto ">
+      <h1 className="">Responsive Animated Cards</h1>
     </div>
   );
 };
