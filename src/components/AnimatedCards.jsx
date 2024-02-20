4054d1849fb7e69c398fadddea4b417f56c7d193
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-20 12:15:27 +0200

    Update AnimatedCards.jsx

--- src/components/AnimatedCards.jsx
+++ src/components/AnimatedCards.jsx
@@ -38,7 +38,10 @@ const AnimatedCards = () => {
       </h1>
 
       {/* cards section */}
-      <div className=""></div>
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center
+      gap-6"
+      ></div>
     </div>
   );
 };
